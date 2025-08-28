3f18ebc39f06147b44803c2ef478a5ddd3420d43
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-08-28 09:14:20 -0400

    update

--- src/js/math/arithmetic/isEven.js
+++ src/js/math/arithmetic/isEven.js
@@ -23,6 +23,12 @@ false
 
 //----//
 
+// % 2 gives the remainder after dividing by 2
+
+// even numbers leave a remainder of 0, so n % 2 === 0 is true only for even numbers
+
+//----//
+
 // Dedicated to God the Father  
 // All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025  
 // https://github.com/ChristopherTopalian  
