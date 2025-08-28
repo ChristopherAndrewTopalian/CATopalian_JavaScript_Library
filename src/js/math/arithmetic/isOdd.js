c3f291fc8b58898007f233a82f66ebac2988dbd4
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-08-28 09:11:15 -0400

    update

--- src/js/math/arithmetic/isOdd.js
+++ src/js/math/arithmetic/isOdd.js
@@ -23,6 +23,12 @@ true
 
 //----//
 
+// % 2 gives the remainder after dividing by 2
+
+// odd numbers leave a remainder of 1, so n % 2 !== 0 is true only for odd numbers
+
+//----//
+
 // Dedicated to God the Father  
 // All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025  
 // https://github.com/ChristopherTopalian  
